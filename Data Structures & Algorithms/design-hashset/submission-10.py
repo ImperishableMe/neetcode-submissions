428f4706243fb4df92437f1ff409c540d3a27890
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2026-05-01 20:29:54 -0700

    Add: design-hashset - submission-10

---
+++ Data Structures & Algorithms/design-hashset/submission-10.py
@@ -0,0 +1,28 @@
+class MyHashSet:
+    N = 10**6
+    BLOCK_SIZE = 128
+    SZ = (N + BLOCK_SIZE - 1) // BLOCK_SIZE
+
+    def __init__(self):
+        print(self.SZ)
+        self.buckets = [0] * (self.SZ)
+    
+    def _get_mask(self, key: int) -> int:
+        return 1 << (key % self.BLOCK_SIZE)
+
+    def add(self, key: int) -> None:
+        bit_index = key // self.BLOCK_SIZE
+        self.buckets[bit_index] |= self._get_mask(key);
+
+    def remove(self, key: int) -> None:
+        self.buckets[key // self.BLOCK_SIZE] &= ~self._get_mask(key);
+
+    def contains(self, key: int) -> bool:
+        return (self.buckets[key // self.BLOCK_SIZE] & self._get_mask(key)) != 0;
+        
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
