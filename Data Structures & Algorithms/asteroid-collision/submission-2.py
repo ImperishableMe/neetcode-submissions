c3f469614429e6573d5dd746c6204a1743b177f7
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2026-05-13 23:43:04 -0700

    Add: asteroid-collision - submission-2

---
+++ Data Structures & Algorithms/asteroid-collision/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        """
+        Two directions:
+        - ast going right, anything on the right (moving opps)
+        """
+        n = len(asteroids)
+        keep = [True] * n
+        stack = []
+
+        for i, ast in enumerate(asteroids):
+           
+           if ast < 0:
+                while stack and asteroids[stack[-1]] < abs(ast):
+                    ind = stack.pop()
+                    keep[ind] = False
+
+                if not stack:
+                    continue
+
+                if asteroids[stack[-1]] == abs(ast):
+                    ind = stack.pop()
+                    keep[ind] = False
+                    keep[i] = False
+                else:
+                    keep[i] = False 
+           else:
+                stack.append(i)
+
+           # while stack and asteroids[stack[-1]] <= 
+        return [
+        val for i, val in enumerate(asteroids) if keep[i]
+        ]
