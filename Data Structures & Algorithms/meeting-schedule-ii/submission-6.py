49d8c3bdeb675907a2d7cd23284c3c27f5b4b85f
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2026-06-09 07:22:50 +0600

    Add: meeting-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-6.py
@@ -0,0 +1,45 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        
+        class Event:
+            OPEN = 1
+            CLOSE = 0
+
+            def __init__(self, ts: int, e_type: int):
+                self.ts = ts
+                self.e_type = e_type
+            
+            def __lt__(self, other: 'Event') -> bool:
+                if self.ts == other.ts:
+                    return self.e_type < other.e_type
+                return self.ts < other.ts
+        
+        events = []
+        for interval in intervals:
+            events.append(Event(interval.start, Event.OPEN))
+            events.append(Event(interval.end, Event.CLOSE))
+        
+        events.sort()
+        
+        max_room_required = 0
+        cur_needed = 0
+
+        for e in events:
+            e_type = e.e_type
+            if e_type == 1:
+                cur_needed += 1
+            else:
+                cur_needed -= 1
+            max_room_required = max(max_room_required, cur_needed)
+
+        return max_room_required
+
+
