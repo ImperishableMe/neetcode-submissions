a2283365aaab6b7ca746f221be30f75653b73038
Bishwajit Bhattacharjee <bishwajit0020@gmail.com>
2026-06-09 19:15:52 +0600

    Add: count-connected-components - submission-4

---
+++ Data Structures & Algorithms/count-connected-components/submission-4.py
@@ -0,0 +1,34 @@
+class DSU:
+    def __init__(self, n):
+        self.n = n
+        self.par = [0] * n
+        self.sz = [1] * n
+        for i in range(n):
+            self.par[i] = i
+    
+    def find(self, node: int):
+        self.par[node] = node if self.par[node] == node else self.find(self.par[node])
+        return self.par[node]
+    
+    def merge(self, u: int, v: int) -> bool:
+        u = self.find(u)
+        v = self.find(v)
+        if u == v:
+            return False
+        if self.sz[u] > self.sz[v]:
+            u, v = v, u
+        
+        self.par[v] = u
+        self.sz[u] += self.sz[v]
+        return True
+
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        dsu = DSU(n)
+        components = n
+
+        for u, v in edges:
+            if dsu.merge(u, v):
+                components -= 1
+        return components
